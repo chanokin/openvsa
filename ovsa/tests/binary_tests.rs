477e04c0435022bf1cc4513a57e10def2960dc10
Garibaldi <chanokin@gmail.com>
2025-12-16 15:18:06 +0000

    more bin tests

--- ovsa/tests/binary_tests.rs
+++ ovsa/tests/binary_tests.rs
@@ -1,15 +1,12 @@
 use rand::seq::index::sample;
-use rand::distr::Uniform;
 use rand::rng;
-
-use ovsa::binary::sparse_random;
 
 
 #[test]
 fn test_sparse_random() {
     let dimension = 10;
     let n_active = 3;
-    let vec = sparse_random(dimension, n_active);
+    let vec = ovsa::binary::sparse_random(dimension, n_active);
     assert_eq!(vec.dim(), dimension);
     assert_eq!(vec.nnz(), n_active);
 }
@@ -53,7 +50,7 @@ fn test_consensus_sum() {
 #[test]
 fn test_consensus_sum_tie() {
     let mut rng  = rng();
-    let dimension = 100;
+    let dimension = 10000;
 
     let mut indices1: Vec<usize> = sample(&mut rng, dimension/2, dimension/4).into_vec();
     indices1.sort();
@@ -72,10 +69,64 @@ fn test_consensus_sum_tie() {
 
     let consensus = ovsa::binary::consensus_sum(&[vec1, vec2]);
     let indices = consensus.indices();
-    println!("Consensus indices: {:?}", indices);
-    for &index in indices {
-         // these are assigned randomly due to tie, how do we test?
-        // assert_eq!(consensus[index], 0); // all positions should be 0 due to tie
-        println!("Index {}: {}", index, consensus[index]);
+
+    println!("Consensus indices: n({}) should be close to {}", indices.len(), dimension / 4);
+
+    let abs_diff = if indices.len() > dimension / 4 {
+        indices.len() - dimension / 4
+    } else {
+        dimension / 4 - indices.len()
+    };
+
+    assert!(abs_diff < dimension / 20, "Consensus sum deviates too much from expected in tie case.");
+
+}
+
+#[test]
+fn test_xor() {
+    let dimension = 10;
+    let vec1 = ovsa::binary::from_indices(dimension, &[1, 3, 5]);
+    let vec2 = ovsa::binary::from_indices(dimension, &[3, 4, 5]);
+    let result = ovsa::binary::xor(&vec1, &vec2);
+    let expected_indices = vec![1, 4];
+    assert_eq!(result.nnz(), expected_indices.len());
+    for &index in &expected_indices {
+        assert_eq!(result[index], 1);
     }
 }
+
+#[test]
+fn test_cyclic_shift() {
+    let dimension = 10;
+    let vec = ovsa::binary::from_indices(dimension, &[1, 3, 5, 9]);
+    let shifted_vec = ovsa::binary::cyclic_shift(&vec, 2);
+    let expected_indices = vec![3, 5, 7, 1];
+    assert_eq!(shifted_vec.nnz(), expected_indices.len());
+    for &index in &expected_indices {
+        assert_eq!(shifted_vec[index], 1);
+    }
+}
+
+#[test]
+fn test_cyclic_shift_negative() {
+    let dimension = 10;
+    let vec = ovsa::binary::from_indices(dimension, &[1, 3, 5, 0]);
+    let shifted_vec = ovsa::binary::cyclic_shift(&vec, -2);
+    let expected_indices = vec![9, 1, 3, 8];
+    assert_eq!(shifted_vec.nnz(), expected_indices.len());
+    for &index in &expected_indices {
+        assert_eq!(shifted_vec[index], 1);
+    }
+}
+
+#[test]
+fn test_similarity() {
+    let dimension = 10;
+    let vec1 = ovsa::binary::from_indices(dimension, &[1, 3, 5]);
+    let vec2 = ovsa::binary::from_indices(dimension, &[3, 4, 5]);
+    let similarity = ovsa::binary::similarity(&vec1, &vec2);
+    // two items are not the same (indices 1 and 4) out of four active indices total
+    let expected_similarity = 1.0 - (2.0 / (dimension as f64));
+    assert_eq!(similarity, expected_similarity); // 2 common active out of 4 total active
+}
+
